ecb56eea4adad44d1089f6c9e87b9c032b8910d4
rjsk-fedora <tegeeyberqayda@aol.com>
2022-08-08 00:06:38 +0900

    Modified header style to assimilate to the City's official site

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="text-gray-600 body-font bg-blue-500">
+    <header className="text-gray-600 body-font bg-white border-b-4 border-solid border-sky-600">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href={'/'} passHref>
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl text-white">My Blog</span>
+          <a className="flex title-font font-medium items-center mb-4 md:mb-0">
+            <span className="ml-3 text-xl">伊佐市こども支援関連情報提供サイト</span>
           </a>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 text-white hover:text-gray-900">Profile</a>
+          <a className="mr-5 hover:text-gray-900">Profile</a>
         </nav>
       </div>
     </header>
